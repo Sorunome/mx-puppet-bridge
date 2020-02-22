aa7db8bf27407bd4d6a4f93748b8da5555346d85
Tulir Asokan <tulir@maunium.net>
2020-02-22 21:43:55 +0200

    Fix copyright header

--- src/provisioningapi.ts
+++ src/provisioningapi.ts
@@ -1,5 +1,5 @@
 /*
-Copyright 2019, 2020 mx-puppet-bridge
+Copyright 2020 mx-puppet-bridge
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
